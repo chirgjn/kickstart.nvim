b7c68a7c4ec4123a611ab5db6a8c95eb732e6b1b
Chirag Jain <chirgjn@users.noreply.github.com>
2026-04-15 03:39:40 +0530

    use 2 spaces for indentation

--- init.lua
+++ init.lua
@@ -101,9 +101,9 @@ vim.g.have_nerd_font = true
 -- Setup indentation
 -- Use spaces
 -- Tab = indentation = 4 spaces
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 -- Make line numbers default
